aea454fa3f4dc37007b404b85ce7257d4c322a4b
Sergio Fuentes <laotrafuente880@gmail.com>
2026-02-27 14:19:49 -0600

    test

--- scripts/Work/Scripts/nueva_correccion.py
+++ scripts/Work/Scripts/nueva_correccion.py
@@ -2,9 +2,26 @@
 
 import sys
 import os
+import subprocess
+import datetime
 
-ESPANSO_MATCH_DIR = os.path.expanduser("~/dotfiles/espanso/.config/espanso/match")
+DOTFILES_DIR = os.path.expanduser("~/dotfiles")
+ESPANSO_MATCH_DIR = os.path.join(DOTFILES_DIR, "espanso/.config/espanso/match")
 CORRECT_FILE = os.path.join(ESPANSO_MATCH_DIR, "correct.yml")
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Uso: python nueva_correccion.py <error> <correccion>")
+        print("Ejemplo: python nueva_correccion. 'peurco' 'puerco'")
+        sys.exit(1)
+
+    commit_date = datetime.datetime.now()
+
+    message = add_entry(sys.argv[1], sys.argv[2])
+
+    auto_commit(message, commit_date)
+    print(f"✅ commited with {message} at {commit_date}")
 
 def add_entry(trigger, replace):
     entry = f"""
@@ -19,15 +36,16 @@ def add_entry(trigger, replace):
 
     with open(CORRECT_FILE, "a") as f:
         f.write(entry)
-    print(f"✅ Agregado: '{trigger}' -> '{replace}'")
 
-# TODO: commit to github with auto save message with the word printed
-def auto_commit(message):
-    pass
+    commit_message = (f"✅ Agregado: '{trigger}' -> '{replace}'")
+    print(commit_message)
+
+    return commit_message
+
+def auto_commit(message, commit_date):
+    subprocess.run(["git", "-c", f"{DOTFILES_DIR}", "-a", "-m", f"{commit_date} - message"])
+    subprocess.run(["git", "push"])
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Uso: python nueva_correccion.py <error> <correccion>")
-        print("Ejemplo: python nueva_correccion. 'peurco' 'puerco'")
-        sys.exit(1)
-    add_entry(sys.argv[1], sys.argv[2])
+    main()
